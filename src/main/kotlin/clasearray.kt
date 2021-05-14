2624a3bd4392590996de74218dc55fe10f27326c
mplacesalvarez <mplacesalvarez@danielcastelaor.org>
2021-05-14 10:51:29 +0200

    Ejercicio 2 subido

--- src/main/kotlin/clasearray.kt
+++ src/main/kotlin/clasearray.kt
@@ -2,4 +2,5 @@
 class clasearray (tam: Int, relleno : (Int) -> Int){
 
     var arr= Array<Int>(size=tam,relleno)
+
 }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,10 +2,9 @@ import kotlin.random.Random
 
 fun main(args: Array<String>) {
 
-
-
     var clarray = clasearray(10){ Random.nextInt(50, 250)}
 
     clarray.arr.forEach { println(it) }
+
 
 }
